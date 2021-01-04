9f605a3ab3dfe0389c6af5aa4b416284fc8cf0cf
Andy <andy27479@gmail.com>
2021-01-04 09:47:14 +0800

    modified multithread

--- com/assignments/IntegerThread.java
+++ com/assignments/IntegerThread.java
@@ -3,46 +3,81 @@ package com.assignments;
 public class IntegerThread
 {
 	private Integer variable;
+	private boolean empty_variable;
 
 	public IntegerThread() {
+		empty_variable = true;
+	}
+
+	synchronized boolean is_empty() {
+		return empty_variable;
+	}
+
+	synchronized void insert(Integer i)
+	{
+		while (!is_empty()) {
+			notify();
+			try {
+				wait(1000);
+			} catch (InterruptedException e) {}
+		}
+		variable = i;
+		empty_variable = false;
+	}
+
+	synchronized Integer extract()
+	{
+		while (is_empty()) {
+			notify();
+			try {
+				wait(1000);
+			} catch (InterruptedException e) {}
+		}
+
+		Integer temp = variable;
 		variable = null;
-	}
-
-	synchronized void set_value(int i)
-	{
-		try {
-			variable = i;
-			// awaken one other waiting Thread
-			notify();
-			// release the monitor
-			wait();
-		} catch (InterruptedException e) {}
-
+		empty_variable = true;
 		notify();
-	}
-
-	synchronized void print_value()
-	{
-		try {
-			while (variable != null) {
-				System.out.println(variable);
-			}
-			System.out.println("null variable");
-			// awaken one other waiting Thread
-			notify();
-			// release the monitor and go to sleep
-			wait();
-		} catch (InterruptedException e) {}
+		return temp;
 	}
 
 	public static void main(String[] args) {
-		Runnable sv = new Thread(new IntegerThread());
-		Runnable pv = new Thread(new IntegerThread());
-		sv.start();
+		IntegerThread thread = new IntegerThread();
+		SetInteger t1 = new SetInteger(thread);
+		PrintInteger t2 = new PrintInteger(thread);
 
+		t1.start();
+		t2.start();
+	}
+}
+
+class SetInteger extends Thread {
+	private IntegerThread thread;
+
+	public SetInteger(IntegerThread thread) {
+		this.thread = thread;
+	}
+
+	public void run() {
 		for (int i=1; i<=10; i++) {
-			sv.set_value(i);
-			pv.print_value();
+			thread.insert(i);
 		}
+	}
+}
+
+class PrintInteger extends Thread {
+	private IntegerThread thread;
+
+	public PrintInteger(IntegerThread thread) {
+		this.thread = thread;
+	}
+
+	public void run() {
+		Integer i;
+
+		do {
+			i = thread.extract();
+			System.out.println("Variable = " + i);
+		} while (i <= 10);
 	}
 }
