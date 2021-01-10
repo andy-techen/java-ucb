55a7baff13061f1228da68cec2aa626cbea2a136
Andy <andy27479@gmail.com>
2021-01-10 10:34:18 +0800

    setup preliminary styles and linked Main with Preferences and Results

--- project/src/main/java/Main.java
+++ project/src/main/java/Main.java
@@ -2,10 +2,12 @@ import javafx.application.Application;
 import javafx.geometry.*;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
+import javafx.scene.image.Image;
 import javafx.scene.layout.*;
 import javafx.stage.Stage;
 
-import java.util.ArrayList;
+import java.io.IOException;
+import java.util.*;
 
 public class Main extends Application {
     Stage window;
@@ -15,6 +17,8 @@ public class Main extends Application {
     @Override
     public void start(Stage primary_stage) {
         window = primary_stage;
+        window.setTitle("FoodMood");
+        window.getIcons().add(new Image("file:resources/logo.png"));
 
         // setting up layout-------------------------------------------------------------
         BorderPane layout = new BorderPane();
@@ -38,6 +42,7 @@ public class Main extends Application {
         search_pane.setSpacing(20);
 
         Label search_label = new Label("FoodMood");
+        search_label.getStyleClass().add("label-title");
         HBox search_box = new HBox();
         search_box.setSpacing(10);
         TextField search_bar = new TextField();
@@ -67,15 +72,60 @@ public class Main extends Application {
         FlowPane results_pane = new FlowPane();
         results_pane.setPadding(new Insets(10, 10, 10, 10));
 
-//        search_button.setOnAction(e -> {
-//            Results.search(term, location, limit);
-//            layout.setCenter(results_pane);
-//        });
+        search_button.setOnAction(e -> {
+            // add mood food to search term
+            String mood = mood_box.getSelectionModel().getSelectedItem().toString();
+            String term = String.join("+", search_bar.getText().split(" ")) + "+" + getMoodFood(mood);
+            System.out.println(term);
+
+            // setting price according to budget
+            int budget = (int) preferences_ls.get(4);
+            String price = getPrice(budget);
+
+            System.out.println(price);
+
+            String location = location_bar.getText();
+            int limit = (int) preferences_ls.get(5);
+            System.out.println(location);
+            System.out.println(limit);
+
+            try {
+                ArrayList<Store> results_arr = Results.search(term, location, price, limit);
+            } catch (IOException ioe) {
+                ioe.printStackTrace();
+            }
+
+            layout.setCenter(results_pane);
+        });
 
         scene = new Scene(layout, 800, 600);
-        window.setTitle("FoodMood");
+        scene.getStylesheets().add("file:resources/style.css");
+
         window.setScene(scene);
         window.show();
+    }
+
+    private String getMoodFood(String mood) {
+        switch (mood) {
+            case "I'm Feeling Happy :)":
+                return String.join("+", (ArrayList) preferences_ls.get(0));
+            case "I'm Feeling Sad :(":
+                return String.join("+", (ArrayList) preferences_ls.get(1));
+            case "I'm Feeling Stressed :/":
+                return String.join("+", (ArrayList) preferences_ls.get(2));
+            default:
+                return String.join("+", (ArrayList) preferences_ls.get(3));
+        }
+    }
+    private String getPrice(int budget) {
+        if (budget <= 10) {
+            return "1";
+        } else if (budget <= 30) {
+            return "1,2";
+        } else if (budget <= 60) {
+            return "1,2,3";
+        }
+        return "1,2,3,4";
     }
 
     public static void main(String[] args) {
